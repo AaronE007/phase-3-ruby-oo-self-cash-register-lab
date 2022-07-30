fd4c9a444b247242d6f9df5f093881b865e09d58
Aaron Elias <aaronelias007@gmail.com>
2022-07-29 21:18:36 -0400

    Test

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,14 @@
+class CashRegister
 
+  attr_accessor :items, :discount, :total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item( title, amount, quantity = 1)
+    self.last_transaction = amount * quantity
+  end 
+
+end 
